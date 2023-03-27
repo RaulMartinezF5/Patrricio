435dd504c4606b40407a2dc4e449b81e60bd1a83
RaulMartinezF5 <raulmartinezalvarez1980@gmail.com>
2023-03-27 09:34:24 +0200

    first commit

---
+++ src/main/java/com/raul/patricio/PatricioApplication.java
@@ -0,0 +1,13 @@
+package com.raul.patricio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatricioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatricioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raul/patricio/PatricioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raul.patricio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatricioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
